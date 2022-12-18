7901334143a98566d8734c98b63fbf99988f2866
Jordi <jordi.montana@gmail.com>
2022-12-19 00:20:32 +0100

    Adding custom api samples

---
+++ tests/PowerAppsBootCamp.Plugins.Tests/PowerAppsBootCamp.Plugins.Tests/CustomApiSumTests.cs
@@ -0,0 +1,20 @@
+﻿using DataverseEntities;
+using Xunit;
+
+namespace FakeXrmEasy.Samples.Plugins.Tests
+{
+    public class CustomApiSumTests : FakeXrmEasyPipelineWithCustomApisTestsBase
+    {
+        [Fact]
+        public void Should_execute_custom_api()
+        {
+            var sumResponse = _service.Execute(new dv_SumRequest()
+            {
+                Prop1 = 7,
+                Prop2 = 3
+            });
+
+            Assert.Equal(10, sumResponse.Results["Sum"]);
+        }
+    }
+}
