565619035ad6eaf71249822d9b3ce91c0f323b52
Jordi <jordi.montana@gmail.com>
2022-12-20 23:36:17 +0100

    Add cutom api + pipeline simulation sample

--- tests/PowerAppsBootCamp.Plugins.Tests/PowerAppsBootCamp.Plugins.Tests/CustomApiSumTests.cs
+++ tests/PowerAppsBootCamp.Plugins.Tests/PowerAppsBootCamp.Plugins.Tests/CustomApiSumTests.cs
@@ -1,10 +1,27 @@
 ﻿using DataverseEntities;
+using FakeXrmEasy.Abstractions.Plugins.Enums;
+using FakeXrmEasy.Pipeline;
+using FakeXrmEasy.Plugins.Audit;
+using FakeXrmEasy.Plugins.PluginSteps;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Linq;
 using Xunit;
 
 namespace FakeXrmEasy.Samples.Plugins.Tests
 {
     public class CustomApiSumTests : FakeXrmEasyPipelineWithCustomApisTestsBase
     {
+        // 100.000
+
+        private class SomePlugin : IPlugin
+        {
+            public void Execute(IServiceProvider serviceProvider)
+            {
+                
+            }
+        }
+
         [Fact]
         public void Should_execute_custom_api()
         {
@@ -15,6 +32,36 @@ namespace FakeXrmEasy.Samples.Plugins.Tests
             }) as dv_SumResponse;
 
             Assert.Equal(10, sumResponse.Sum);
+        }
+
+        [Fact]
+        public void Should_trigger_another_plugin_registered_against_the_custom_api()
+        {
+            var request = new dv_SumRequest()
+            {
+                Prop1 = 7,
+                Prop2 = 3
+            };
+
+            _context.RegisterPluginStep<SomePlugin>(new PluginStepDefinition()
+            {
+                MessageName = request.RequestName,
+                Stage = ProcessingStepStage.Postoperation
+            });
+
+            var sumResponse = _service.Execute(request) as dv_SumResponse;
+
+            Assert.Equal(10, sumResponse.Sum);
+
+            var pluginStepAudit = _context.GetPluginStepAudit();
+            var stepsAudit = pluginStepAudit.CreateQuery().ToList();
+
+            Assert.Single(stepsAudit);
+            var auditedStep = stepsAudit[0];
+
+            Assert.Equal(request.RequestName, auditedStep.MessageName);
+            Assert.Equal(ProcessingStepStage.Postoperation, auditedStep.Stage);
+            Assert.Equal(typeof(SomePlugin), auditedStep.PluginAssemblyType);
         }
     }
 }
