563c94041a12be467057ebee67438c0302c40864
bwmodular <ben@nortica.co.uk>
2023-02-08 23:19:04 +0000

    Demonstrate issue with exceptions in Pipeline simulation

---
+++ spkl/MyPluginsSample/PluginWhichThrowsAnException.cs
@@ -0,0 +1,29 @@
+﻿using DataverseEntities;
+using Microsoft.Xrm.Sdk;
+using System;
+
+namespace FakeXrmEasy.Samples.PluginsWithSpkl
+{
+    [CrmPluginRegistration(message: MessageNameEnum.Create,
+    entityLogicalName: Contact.EntityLogicalName,
+    stage: StageEnum.PreValidation,
+    executionMode: ExecutionModeEnum.Synchronous,
+    filteringAttributes: "",
+    stepName: "PluginWhichThrowsAnException",
+    executionOrder: 1,
+    isolationModel: IsolationModeEnum.Sandbox,
+    Description = "This plugin throws an exception ",
+    Id = "d2231f29-dc15-48ee-ae56-98b2d13473cd"
+)]
+    // Plugin which just thorws an exception - created for the purposes of testing this issue:
+    // https://github.com/DynamicsValue/fake-xrm-easy/issues/85
+    public class PluginWhichThrowsAnException : IPlugin
+    {
+        public const string PluginExceptionMessage = "An error occured";
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            throw new InvalidPluginExecutionException(PluginExceptionMessage);
+        }
+    }
+}
---
+++ spkl/MyPluginsSampleTests/TestExceptionsThrownInPipeline.cs
@@ -0,0 +1,50 @@
+﻿using DataverseEntities;
+using FakeXrmEasy.Plugins;
+using FakeXrmEasy.Samples.PluginsWithSpkl;
+using Xunit;
+using Microsoft.Xrm.Sdk;
+using System;
+
+namespace MyPluginsSampleTests
+{
+    // Tests developed for this issue
+    // https://github.com/DynamicsValue/fake-xrm-easy/issues/85
+
+    public class TestExceptionsThrownInPipeline : FakeXrmEasyAutomaticRegistrationTestsBase
+    {
+        // This test passes as the plugin is invoked directly
+        [Fact]
+        public void When_A_Resource_Request_Is_Created_It_Will_Get_Its_Name_From_Its_Related_Opportunity_And_Team()
+        {
+            // Arrange
+            Contact target = new Contact() { Id = Guid.NewGuid() };
+            XrmFakedPluginExecutionContext pluginExecutionContext = _context.GetDefaultPluginContext();
+            ParameterCollection _inputParameters = new ParameterCollection { { "Target", target } };
+            pluginExecutionContext.InputParameters = _inputParameters;
+            pluginExecutionContext.Stage = (int)StageEnum.PreValidation;
+            pluginExecutionContext.MessageName = MessageNameEnum.Create.ToString();
+
+            //Act
+            Action act = () => _context.ExecutePluginWith<PluginWhichThrowsAnException>(pluginExecutionContext);
+            
+            //Assert
+            InvalidPluginExecutionException exception = Assert.Throws<InvalidPluginExecutionException>(act);            
+            Assert.Equal(PluginWhichThrowsAnException.PluginExceptionMessage, exception.Message);
+
+        }
+
+        // This test fails - because the plugin is invoked through pipeline simulation, not directly
+        [Fact]
+        public void When_A_Resource_Request_Is_Created_Without_A_Team_An_Exception_Will_Get_Thrown()
+        {
+            // Arrange
+            Contact contact = new Contact() { Id = Guid.NewGuid() };
+            // Act
+            Action act = () => _service.Create(contact);
+            //assert
+            InvalidPluginExecutionException exception = Assert.Throws<InvalidPluginExecutionException>(act);
+            Assert.Equal(PluginWhichThrowsAnException.PluginExceptionMessage, exception.Message);            
+        }
+
+    }
+}
